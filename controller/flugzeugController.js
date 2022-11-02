3e8664db27cec9c79d1b292ae53b517fe8165ad8
Magdalena Cale <107847566+MagdaCale@users.noreply.github.com>
2022-11-02 10:46:39 +0100

    getFlugzeug Controller

---
+++ controller/flugzeugController.js
@@ -0,0 +1,12 @@
+
+
+export const getFlugzeug = async (req, res) => {
+    try {
+        const flugzeug = await getAllFlugzeug()
+        console.log(flugzeug)
+        res.status(200).json(flugzeug)
+    }catch(error){
+        console.log(error)
+        res.status(500).json({error: error})
+    }
+}
