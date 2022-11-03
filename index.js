c183ae3eb3f9b2116b1d1b8a225e10e1e01d6f7e
artem <bakirovartem69@gmail.com>
2022-11-03 11:24:13 +0100

    hallo

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ import express from "express"
 import './config.js'
 import { getFlugzeug, addFlugzeug } from "./controller/flugzeugController.js"
 
+
+//packages
+import cors from "cors";
+
 const PORT = 9090
 const app = express()
+
+//middleware
+app.use(cors({origin: "http://localhost:3000"}));
 
 app.listen(PORT, () => console.log("huhu", PORT))
 app.use(express.json())
